4dde900e662f4398e1dca732a5479884eb0c7f0b
SydePrince <SydePrince@126.com>
2016-07-28 19:34:16 +0800

    7-28 19.34 王卓

---
+++ enjoytheworld/src/main/java/com/three/enjoytheworld/HandPick_ShowItem_Activity.java
@@ -0,0 +1,13 @@
+package com.three.enjoytheworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandPick_ShowItem_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hand_pick__show_item_activity);
+    }
+}
